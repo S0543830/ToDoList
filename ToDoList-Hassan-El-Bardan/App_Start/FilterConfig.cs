9f47f1ff27efd2d64717b2824960a5005f4b2a6e
S0543830 <s0543830@htw-berlin.de>
2018-04-03 17:39:42 +0200

    Add new ASP.net MVC Project

---
+++ ToDoList-Hassan-El-Bardan/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ToDoList_Hassan_El_Bardan
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ToDoList-Hassan-El-Bardan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToDoList_Hassan_El_Bardan.MvcApplication" Language="C#" %>
